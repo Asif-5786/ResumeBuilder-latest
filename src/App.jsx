20de784063d18f9382207932cac80f076e420d8a
rehmanasif1009 <rehmanasif1009@gmail.com>
2024-07-24 15:47:08 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Body from './components/Body/body'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Body />
+    </div>
+  )
+}
+
+export default App
